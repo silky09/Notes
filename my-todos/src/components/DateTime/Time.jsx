6ce0e81b19ac2069499a22109d4680c2ecc15dcc
silky <silkysantosh09@gmail.com>
2021-04-29 11:38:34 +0200

    Added logo in the header.

--- my-todos/src/components/DateTime/Time.jsx
+++ my-todos/src/components/DateTime/Time.jsx
@@ -6,26 +6,27 @@ const Time = () => {
   // const monthNames = ["January", "February", "March", "April", "May", "June",
   //   "July", "August", "September", "October", "November", "December"];
   
-  const monthNames = myDate.toLocaleString('default', { month: 'long' });
   const day = myDate.getDate();
+  const monthNames = myDate.toLocaleString('default', { month: 'long' });
   const year = myDate.getFullYear();
   // const month = monthNames[myDate.getMonth()];
   //const date = myDate.toLocaleDateString();
   
   //const time = myDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: false});
+  
   // used hook for adding live clock
-  let now = new Date().toLocaleTimeString();
+  let now = new Date().toLocaleTimeString('en-GB');
 
   const [time, setTime] = useState(now);
 
   const updateTime = () => {
-  now = new Date().toLocaleTimeString();
+  now = new Date().toLocaleTimeString('en-GB');
   setTime(now);
   }
   setInterval(updateTime, 1000);
+
   
   const currentHours = myDate.getHours();
-
   let message = "";
   
   const colorChange = {
@@ -37,7 +38,7 @@ const Time = () => {
     colorChange.color = "red";
   } else if (currentHours < 18) {
     message = "Good Afternoon";
-    colorChange.color = "green";
+    colorChange.color = "purple";
   } else {
     message = "Good Evening";
     colorChange.color = "blue";
@@ -46,7 +47,7 @@ const Time = () => {
   return (
     <div>
      <h4>Hi, <span style={colorChange}>{message}</span> 😊 </h4>
-    <h4>It is currently  <span style={colorChange}>{time} </span> , {day}  {monthNames} {year} </h4> 
+    <h4>It is currently  <span style={colorChange}>{time}</span>, {day} {monthNames} {year} </h4> 
     </div>
   )
 }
--- my-todos/src/components/Header/Header.jsx
+++ my-todos/src/components/Header/Header.jsx
@@ -3,12 +3,15 @@ import './header.css'
 import Time from '../DateTime/Time'
 
 const Header = () => {
-
-
+     
+  const logo = <img className="header_logo" src="https://i.pinimg.com/originals/16/3a/a6/163aa6aa75341681c25b0160bc67a306.png" alt="task_logo" width="70" height="50" />;
     return (
         <div>
-          <header>
-              <h1>Notes📝</h1>
+        <header>
+          
+          {logo}   
+          <h1>Notes</h1>
+          
           </header> 
           <br />
            <hr /> 
